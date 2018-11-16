3009ec91b724daad96ef3d20bbb4b2e0565a2e2a
adsv <avidal@csu.fullerton.edu>
2018-11-16 15:47:33 -0800

    Wrote the test routine. Committed the data before the test because I'm dumb.

--- experiment.cc
+++ experiment.cc
@@ -8,11 +8,23 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
 
 #include "project2.hh"
 #include "timer.hh"
 
 using namespace std;
+
+string_vector getSubset(string_vector words,int start,int size){
+  //make sure we don't go off the edge of the words vector
+  if((start + size) > words.size()-1){
+    start -= (start + size - words.size() + 1);
+  }
+
+  return string_vector(words.begin() + start, words.begin() + start + size);
+}
 
 int main() {
 
@@ -22,18 +34,66 @@ int main() {
     return 1;
   }
 
-  int n = 1000;
-  string_vector n_words(all_words.begin(), all_words.begin() + n);
+  srand(time(NULL));
 
-  randomize_list(n_words);
-
+  int i;
+  int n = 50000;
+  int iterations = 5000;
+  int totalSize = all_words.size();
+  int randStart;
   Timer timer;
-  mergesort(n_words);
-  double elapsed = timer.elapsed();
-  cout << "mergesort, "
-       << "n=" << n << ", "
-       << "elapsed time = " << elapsed << " seconds" << endl;
+  double elapsed[iterations][2] = {0.0};
+  string_vector n_words;
+    
+  cout << "Words=" << n << ", "<< "iteration=" << iterations << endl;
+  cout << "Iteration,\tMergeTime,\tQSTime" << endl;
+  
+  for(i = 0; i < iterations; i++){
+    cout << i << ",\t";
+    
+    //Get the merge sort time
+    randStart = rand() % totalSize;
+    n_words = getSubset(all_words, randStart, n);
+    randomize_list(n_words);
+    timer.reset();
+    mergesort(n_words);
+    elapsed[i][0] = timer.elapsed();
+    cout << elapsed[i][0] << ",\t";
 
+    //Get the quick sort time
+    randStart = rand() % totalSize;
+    n_words = getSubset(all_words, randStart, n);
+    randomize_list(n_words);
+    timer.reset();
+    quicksort(n_words);
+    elapsed[i][1] = timer.elapsed();
+    cout << elapsed[i][1] << endl;
+      }
+
+  double sumMerge, sumQuick, meanMerge, meanQuick, stdMerge, stdQuick = 0.0;
+
+  for(i = 0; i < iterations; i++){
+    sumMerge += elapsed[i][0];
+    sumQuick += elapsed[i][1];
+  }
+
+  meanMerge = sumMerge / iterations;
+  meanQuick = sumQuick / iterations;
+
+  sumMerge = sumQuick = 0;
+  for(i = 0; i < iterations; i++){
+    sumMerge += (elapsed[i][0] - meanMerge) * (elapsed[i][0] - meanMerge);
+    sumQuick += (elapsed[i][1] - meanQuick) * (elapsed[i][1] - meanQuick);
+  }
+
+  stdMerge = sqrt(sumMerge/(iterations-1));
+  stdQuick = sqrt(sumQuick/(iterations-1));
+
+  cout << endl << "Mean MS Time= " << meanMerge << " Std of MS Time="
+       << stdMerge << endl;
+  cout << "Mean QS Time= " << meanQuick << " Std of QS Time="
+       << stdQuick << endl;
+  
   return 0;
 }
 
